beac1281060a7df5cd67ad01ee8ea07d1c954a53
Weng Xuetian <wengxt@gmail.com>
2023-03-25 21:24:25 -0700

    Add im module probing program

---
+++ gtk2/immodule-probing.cpp
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2023~2023 CSSlayer <wengxt@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ */
+#include <gtk/gtk.h>
+#include <gtk/gtkimmodule.h>
+#include <iostream>
+
+int main() {
+    GtkIMContext *context;
+    char *preedit_string = NULL;
+    PangoAttrList *preedit_attrs = NULL;
+    const char *context_id;
+
+    gtk_init();
+    context = gtk_im_multicontext_new();
+    gtk_im_context_get_preedit_string(context, &preedit_string, &preedit_attrs,
+                                      0);
+    context_id =
+        gtk_im_multicontext_get_context_id(GTK_IM_MULTICONTEXT(context));
+    std::cout << "GTK_IM_MODULE=" << context_id << std::endl;
+    return 0;
+}
---
+++ gtk3/immodule-probing.cpp
@@ -0,0 +1,1 @@
+../gtk2/immodule-probing.cpp
